a85d5f0b0ca3225ee3471a2db55d6aaae81c0617
remusezequiel <ezequielremus@gmail.com>
2018-07-03 12:14:41 -0300

    ejercicio 3 js

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,9 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	var nombre;
+    nombre = document.getElementById("elnombre").value="pepe";//variable
+    alert(nombre);//funcion
 }
 
 
