72bb11294e194e03f5cfb54961ee530934b84b03
remusezequiel <ezequielremus@gmail.com>
2018-07-03 12:04:23 -0300

    ejercicios 1 y 2

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,11 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	
+    var mensaje="Esto funciona de maravilla";
+    alert(mensaje);
+    
 }
-
+/**Asi es la notacion para hacer comentarios de mas de una linea
+ * por cadalinea se agraga un asterisco al principio
+ * 
+ */
