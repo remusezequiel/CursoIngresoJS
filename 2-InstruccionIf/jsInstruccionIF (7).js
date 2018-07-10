981a648bfb1b9eee5988d8ae385047231147bb28
remusezequiel <ezequielremus@gmail.com>
2018-07-10 12:08:22 -0300

    ejercicio 7

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -3,6 +3,6 @@ function mostrar()
 //tomo la edad  
     var edad = document.getElementById("edad").value;
 	var ec = document.getElementById("estadoCivil").value;
-     if(edad < 18 ){}else{if(ec == "soltero"){alert("Es muy pequeño ara no ser soltero")}};
-     
+     if(edad < 18 && ec != "soltero"){alert("uahfsuwha")};
+
 }//FIN DE LA FUNCIÓN
