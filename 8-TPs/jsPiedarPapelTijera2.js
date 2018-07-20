8534994426008a967a659fc4c56260716e8834a6
remusezequiel <40795786+remusezequiel@users.noreply.github.com>
2018-07-20 18:25:53 -0300

    20.07

--- 8-TPs/jsPiedarPapelTijera2.js
+++ 8-TPs/jsPiedarPapelTijera2.js
@@ -6,8 +6,8 @@ var ContadorDePerdidas=0;
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 3
-	 	
-
+	eleccionMaquina = Math.floor(Math.random()*4);
+	console.log(eleccionMaquina);
 
 
 }//FIN DE LA FUNCIÓN
