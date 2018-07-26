affbf3279d0128a4dd075413ac605ed7f27a3445
remusezequiel <ezequielremus@gmail.com>
2018-07-26 12:17:40 -0300

    26.07

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,13 @@
-/** */
+/**Enunciado:
+Bienvenidos. 
+(SWITCH)pedir el ingreso de un planeta del sistema solar 
+Si es la tierra mostrar "acá vivimos". 
+Si está más cerca del sol, "acá hace más calor". 
+Si está más lejos del sol, "acá hace más frio". 
+Si no es un planeta valido informarlo. 
+Aclaración: los planetas se ingresan en minúscula 
+(Mercurio y Venus están antes que la tierra).
+ */
 function mostrar()
 {
     var p = prompt("Ingrese el nombre de un planeta del sistem solar");
@@ -24,6 +33,7 @@ function mostrar()
         break;
         //---------------------------------
         default :
-            alert('No es un planeta del sistema solar'); 
+            alert('No es un planeta del sistema solar');
+            p = prompt("Ingrese el nombre de un planeta del sistem solar");
     }
 }
